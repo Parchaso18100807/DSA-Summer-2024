8a4274bcc06d64d699ff8557914d36feffa90a75
unknown <josephparchaso@gmail.com>
2024-07-05 14:33:55 +0800

    BST init commit

---
+++ 5 BST/BST.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "BST.h"
+
+void initTree(NodePtr* TREE){
+	
+	
+}
+
+void add(NodePtr* TREE, Product item){
+	
+	
+}
+
+void delete(NodePtr* TREE, char prodName[]){
+	
+	
+}
+
+void BFS(NodePtr TREE){
+	
+	
+}
+
+void inOrder(NodePtr TREE){
+	
+	
+}
+
+void postOrder(NodePtr TREE){
+	
+	
+}
+
+void preOrder(NodePtr TREE){
+	
+	
+}
+
+
